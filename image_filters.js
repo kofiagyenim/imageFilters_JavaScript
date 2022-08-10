621767f2ce6f318ffad95a313dc71473f7b44ae2
kofiagyenim <110791379+kofiagyenim@users.noreply.github.com>
2022-08-10 18:50:49 -0400

    updated file

--- image_filters.js
+++ image_filters.js
@@ -1,6 +1,6 @@
 <script src="https://www.dukelearntoprogram.com/course1/common/js/image/SimpleImage.js" >
     //I used Duke's Learn To Program utility class Simple Image to implement these
-    //It is in no way mine lol
+ 
 
  //this function makes an image red by increasing the R value in the RGB of a pixel 
 function makeredlarge(imagename) {
@@ -11,8 +11,7 @@ function makeredlarge(imagename) {
     return image
 }
 
-// this function removes or sets the Red value of the pixel to 0, hence creating a greenish-blue Hue (i think that's how I could call it
-//I was never good with colors lol)
+//removes all red pixels(sets them to their least value to re-create a greenish-blue hue
 function removeallred(imagename){
     var image = new SimpleImage(imagename);
     for(var i of image.values()){
